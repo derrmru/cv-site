f99e2f93296dbd42622978adce50c59341ce74d9
derrmru <podogomacbookair@MacBook-Air.local>
2020-11-02 10:44:30 +0100

    seo on thank you message

--- src/pages/submitted.js
+++ src/pages/submitted.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import Body from '../templates/Body/Body'
+import SEO from '../components/SEO/SEO'
 
 const Submitted = () => {
     return (
         <Body>
+            <SEO title="Thanks for getting in touch." />
             <div>
                 <h1>Email Submitted</h1>
                 Thanks for getting in touch. You'll hear from me shortly!
